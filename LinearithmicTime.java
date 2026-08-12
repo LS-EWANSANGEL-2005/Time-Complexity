816924f5373850b7ae6c2958ff8018ca379d5459
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-12 11:46:36 +0530

    Add files via upload

---
+++ LinearithmicTime.java
@@ -0,0 +1,15 @@
+package TimeComplexity;
+
+public class LinearithmicTime {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j=j*3) {
+				System.out.println(i+""+j);
+			}
+		}
+
+	}
+
+}
