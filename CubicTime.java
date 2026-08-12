3baac7211da5f05d15b595db2e76f5ca313bf064
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-12 11:45:47 +0530

    Add files via upload

---
+++ CubicTime.java
@@ -0,0 +1,17 @@
+package TimeComplexity;
+
+public class CubicTime {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				for(int k=1;k<=n;k++) {
+					System.out.println(i+""+j+""+k);
+				}
+			}
+		}
+
+	}
+
+}
