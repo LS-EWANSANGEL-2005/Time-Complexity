00a1d92f91a9afe550313b39061a1626bfde1577
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-12 11:47:06 +0530

    Add files via upload

---
+++ LogorithmeticTime.java
@@ -0,0 +1,13 @@
+package TimeComplexity;
+
+public class LogorithmeticTime {
+
+	public static void main(String[] args) {
+		int n=100;
+		for(int i=1;i<=n;i=i*3) {
+			System.out.println(i+"");
+		}
+
+	}
+
+}
